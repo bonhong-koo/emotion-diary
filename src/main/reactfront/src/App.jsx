175c92cdd0bf30841d5b5f8ad09e899ae629ab89
koo <qhsghddl33@naver.com>
2025-10-15 16:25:18 +0900

    App.jsx 대소문자 변경

--- src/main/reactfront/src/App.jsx
+++ src/main/reactfront/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import New from "./pages/New";
 import Regist from "./pages/Regist";
 import Diary from "./pages/Diary";
